392ada03f29341145030272e8fc726e144083a24
Rokit Hasan <lotifnazma@gmail.com>
2022-09-18 03:37:45 +0600

    flight and hotel section

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ document.querySelectorAll(".tabs").forEach(tabsContainer => {
 
     tabsContainer.querySelector(".tabs_-sidebar .tabs__button").click();
 });
+
+
+var splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: 3,
+    rewind: true,
+    breakpoints: {
+        640: {
+            perPage: 2,
+            gap: '.7rem',
+            height: '12rem',
+        },
+        480: {
+            perPage: 1,
+            gap: '.7rem',
+            height: '12rem',
+        },
+    },
+});
+splide.mount();
